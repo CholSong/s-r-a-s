5a68e9efe82e90c55b72488a0d9daa8cdef892eb
Evandro Carrenho <evandro@bemtevi-mobile.com>
2011-08-31 08:56:56 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BemteviEcomm::Application.config.secret_token = '531c3e9f582f79e84d1647cc11d55ea67415e3222bf9e2750015e5f92841fb2c140d51a7ca9319388b3264fe09516e635d6bc319845efea2ccfca375570bd249'
