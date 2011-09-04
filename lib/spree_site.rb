ca759a76f8d192eb24d67984cb3197b1fafa712d
Evandro Carrenho <evandro@bemtevi-mobile.com>
2011-09-04 18:39:49 -0300

    Adding pt-BR locale.

--- lib/spree_site.rb
+++ lib/spree_site.rb
@@ -11,7 +11,7 @@ module SpreeSite
 
       Spree::Config.set :site_name => 'Bemtevi Mobile',
       :site_url => 'bemtevi-mobile.com',
-      :default_locale => 'pt',
+      :default_locale => 'pt-BR',
       :allow_locale_switching => false,
       :default_country_id => 28,
       :logo => '/images/bemtevi/bemtevi_logo.png',
