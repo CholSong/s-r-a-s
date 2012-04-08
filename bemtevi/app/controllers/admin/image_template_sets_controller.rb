82a18dca92e92f10eb07e8722da0f76e6daca970
Evandro Carrenho <evandro@bemtevi-mobile.com>
2012-04-08 13:50:58 -0300

    Fixes on the template creation screen according to Francois feedback.

--- bemtevi/app/controllers/admin/image_template_sets_controller.rb
+++ bemtevi/app/controllers/admin/image_template_sets_controller.rb
@@ -3,11 +3,11 @@ class Admin::ImageTemplateSetsController < Admin::ResourceController
   def new
     summary_template = @object.image_templates.build :template_type => "summary"
     summary_template.build_background_image
-    fill_missing_overlays summary_template
+    fill_missing_summary_overlays summary_template
     
     detail_template = @object.image_templates.build :template_type => "detail"
     detail_template.build_background_image
-    fill_missing_overlays detail_template
+    fill_missing_detail_overlays detail_template
 
     super
   end
@@ -17,7 +17,11 @@ class Admin::ImageTemplateSetsController < Admin::ResourceController
       if template.background_image.nil?
         template.build_background_image
       end
-      fill_missing_overlays template
+      if template.template_type == "summary"
+        fill_missing_summary_overlays template
+      else
+        fill_missing_detail_overlays template
+      end
     }
     
     super
@@ -40,26 +44,41 @@ class Admin::ImageTemplateSetsController < Admin::ResourceController
 
   private
   
-  def fill_missing_overlays(image_template)
-    missing_image_overlays = 2
-    missing_text_overlays = 2
-    
-    if !image_template.overlays.nil?
-      image_template.overlays.each { |overlay|
-        missing_image_overlays -= 1 if !overlay.image_overlay.nil?
-        missing_text_overlays -= 1 if !overlay.text_overlay.nil?
-      }
+  def fill_missing_summary_overlays(image_template)
+    product_image_overlay = image_template.overlays.select {|overlay| overlay.tag == "product_image"}[0]
+    if product_image_overlay.nil?
+      product_image_overlay = image_template.overlays.build :tag => "product_image"
+    end
+    image_overlay = product_image_overlay.image_overlay
+    image_overlay = product_image_overlay.build_image_overlay if image_overlay.nil?
+    image_overlay.build_overlay_image if image_overlay.overlay_image.nil?
+
+    promotion_title_overlay = image_template.overlays.select {|overlay| overlay.tag == "title"}[0]
+    if promotion_title_overlay.nil?
+      promotion_title_overlay = image_template.overlays.build :tag => "title"
+      promotion_title_overlay.build_text_overlay
+    end
+  end
+
+  def fill_missing_detail_overlays(image_template)
+    product_image_overlay = image_template.overlays.select {|overlay| overlay.tag == "product_image"}[0]
+    if product_image_overlay.nil?
+      product_image_overlay = image_template.overlays.build :tag => "product_image"
+    end
+    image_overlay = product_image_overlay.image_overlay
+    image_overlay = product_image_overlay.build_image_overlay if image_overlay.nil?
+    image_overlay.build_overlay_image if image_overlay.overlay_image.nil?
+
+    promotion_title_overlay = image_template.overlays.select {|overlay| overlay.tag == "title"}[0]
+    if promotion_title_overlay.nil?
+      promotion_title_overlay = image_template.overlays.build :tag => "title"
+      promotion_title_overlay.build_text_overlay
     end
 
-    missing_image_overlays.times do
-      overlay = image_template.overlays.build
-      image_overlay = overlay.build_image_overlay
-      image_overlay.build_overlay_image
-    end
-    
-    missing_text_overlays.times do
-      overlay = image_template.overlays.build
-      overlay.build_text_overlay
+    promotion_details_overlay = image_template.overlays.select {|overlay| overlay.tag == "details"}[0]
+    if promotion_details_overlay.nil?
+      promotion_details_overlay = image_template.overlays.build :tag => "details"
+      promotion_details_overlay.build_text_overlay
     end
   end
 
