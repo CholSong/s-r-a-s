7f64cd74bf4e14aea9ff4856747af5797369c5ac
John Pray <louiegeetoo@gmail.com>
2012-07-10 10:34:54 -0400

    Add check for Spree presence before config to allow initial migration to proceed

--- lib/spree_site.rb
+++ lib/spree_site.rb
@@ -9,22 +9,24 @@ module SpreeSite
         Rails.env.production? ? require(c) : load(c)
       end
 
-      Spree::Config.set :site_name => 'Bemtevi Mobile',
-      :site_url => 'bemtevi-mobile.com',
-      :default_locale => 'pt-BR',
-      #:default_locale => 'en-US',
-      :allow_locale_switching => false,
-      :default_country_id => 28,
-      :logo => '/images/bemtevi/bemtevi_logo.png',
-      :admin_interface_logo => 'bemtevi/bemtevi_logo.png',
-      # Bemtevi
-      # :full_admin_ui - This configuration toggles between the full admin UI and the
-      # simplified one. The simplified UI is currently used on production, untill we
-      # have the merchant UI for creating promotions.
-      :full_admin_ui => false
+      if Spree::Config.instance
+        Spree::Config.set :site_name => 'Bemtevi Mobile',
+        :site_url => 'bemtevi-mobile.com',
+        :default_locale => 'pt-BR',
+        #:default_locale => 'en-US',
+        :allow_locale_switching => false,
+        :default_country_id => 28,
+        :logo => '/images/bemtevi/bemtevi_logo.png',
+        :admin_interface_logo => 'bemtevi/bemtevi_logo.png',
+        # Bemtevi
+        # :full_admin_ui - This configuration toggles between the full admin UI and the
+        # simplified one. The simplified UI is currently used on production, untill we
+        # have the merchant UI for creating promotions.
+        :full_admin_ui => false
 
-      s3_config = YAML.load(File.read(Rails.root + 'config' + 's3.yml'))[Rails.env].with_indifferent_access
-      Spree::Config.set s3_config
+        s3_config = YAML.load(File.read(Rails.root + 'config' + 's3.yml'))[Rails.env].with_indifferent_access
+        Spree::Config.set s3_config
+      end
 
     end
     
